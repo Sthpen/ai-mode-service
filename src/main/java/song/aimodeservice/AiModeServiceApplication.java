0ba5f5624cd24c69f18d8e670b45dd058a29dd3f
吴佑 <1824354663@qq.com>
2025-03-29 18:56:53 +0800

    初始化项目

---
+++ src/main/java/song/aimodeservice/AiModeServiceApplication.java
@@ -0,0 +1,14 @@
+package song.aimodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiModeServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AiModeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/song/aimodeservice/AiModeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package song.aimodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiModeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
